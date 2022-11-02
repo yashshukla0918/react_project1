d4511f11768ec16331eb3aa3990b5bed93138406
Yash Kumar Shukla <69341705+yashshukla0918@users.noreply.github.com>
2022-11-02 16:27:03 +0530

    Cookie Storage enabled

--- todos-list/src/App.js
+++ todos-list/src/App.js
@@ -5,14 +5,20 @@ import Footer from './components/Footer';
 import Todos from './components/Todos';
 import { useState } from 'react';
 
+import { useCookies } from 'react-cookie';
+
+
+
 
 
 function App() {
 
+  
   const onDelete = (todo) => {
     setTask_to_be_done(task_to_de_done.filter((e)=>{
       return e!==todo;
     }));
+    
   }
 
   const addTask = (title,desc)=>{
@@ -35,39 +41,13 @@ function App() {
     }
     let temp = task_to_de_done.slice();
     temp.splice(task_to_de_done.indexOf(todo),1,editedTask);
-    setTask_to_be_done(temp);
-    
-
+    setTask_to_be_done(temp);setCookies("MyData",task_to_de_done);
   }
+  const [cookies, setCookies,removeCookies] = useCookies([]);
+  const [task_to_de_done, setTask_to_be_done] = useState(cookies['MyData']);
+  setCookies("MyData",task_to_de_done);
 
-
-  const [task_to_de_done, setTask_to_be_done] = useState(
-    [
-      // {
-      //   sno: 0,
-      //   title: "Task 1",
-      //   desc: "Make a quick report of Employee"
-      // },
-
-      // {
-      //   sno: 1,
-      //   title: "Task 2",
-      //   desc: "Divide all dividend to the stack holder"
-      // }
-      // ,
-      // {
-      //   sno: 2,
-      //   title: "Task 3",
-      //   desc: "Make a excel sheet for eye patients"
-      // }
-      // ,
-      // {
-      //   sno: 3,
-      //   title: "Task 4",
-      //   desc: "Make sure every doctor is comfortable here"
-      // }
-    ]
-  );
+  
 
   return (
     <>
